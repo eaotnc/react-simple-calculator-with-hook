5baa3742a7538f178685c260dd7b71fa779eeafe
Thanatcha <bonsai.eao@gmail.com>
2019-09-09 17:54:57 +0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [output, setOutput] = useState('');
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
        calcucalor
        <div className="calculator">
-         <input className="output" type="number"/>
+         <input className="output" type="string" value={output}/>
         <div className="numpad">
-          <button className="button">9</button>
-          <button className="button">8</button>
-          <button className="button">7</button>
-          <button className="button">6</button>
-          <button className="button">5</button>
-          <button className="button">4</button>
-          <button className="button">3</button>
-          <button className="button">2</button>
-          <button className="button">1</button>
-          <button className="button">0</button>
-          <button className="button">.</button>
+          <button className="button" onClick={()=>setOutput(output+'9')} >9</button>
+          <button className="button"  onClick={()=>setOutput(output+'8')} >8</button>
+          <button className="button"  onClick={()=>setOutput(output+'7')} >7</button>
+          <button className="button"  onClick={()=>setOutput(output+'6')} >6</button>
+          <button className="button"  onClick={()=>setOutput(output+'5')} >5</button>
+          <button className="button"  onClick={()=>setOutput(output+'4')} >4</button>
+          <button className="button"  onClick={()=>setOutput(output+'3')} >3</button>
+          <button className="button"  onClick={()=>setOutput(output+'2')} >2</button>
+          <button className="button"  onClick={()=>setOutput(output+'1')} >1</button>
+          <button className="button"  onClick={()=>setOutput(output+'0')} >0</button>
+          <button className="button"  onClick={()=>setOutput(output+'.')} >.</button>
+          <button className="button"  onClick={()=>setOutput('')} >C</button>
         </div>
         <div className="operator">
-          <button className="button">+</button>
-          <button className="button">-</button>
-          <button className="button">X</button>
-          <button className="button">/</button>
+          <button className="button"onClick={()=>setOutput(output+'+')} >+</button>
+          <button className="button"onClick={()=>setOutput(output+'-')} >-</button>
+          <button className="button"onClick={()=>setOutput(output+'*')} >X</button>
+          <button className="button"onClick={()=>setOutput(output+'/')} >/</button>
         </div>
+        <div className="enter" onClick={()=>setOutput(eval(output))}> Enter</div>
+
       </div>
       </header>
     </div>
