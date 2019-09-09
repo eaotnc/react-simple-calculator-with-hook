e09ea6f3c3d8f2c28d268e0a6bf62cf4a10b8a3c
Thanatcha <bonsai.eao@gmail.com>
2019-09-09 17:57:42 +0700

    delete unuse line

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React,{useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [output, setOutput] = useState('');
-  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
